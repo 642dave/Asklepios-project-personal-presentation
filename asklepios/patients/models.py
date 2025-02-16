587390ed614c69de67e5b8f88bb1ab6d20421b15
David Rakovsky <david.rakovsky@centrum.cz>
2025-02-16 08:53:41 +0100

    in posgresql created database asklepios with table patients_patient with basic data, all connected with vs code in settings

--- asklepios/patients/models.py
+++ asklepios/patients/models.py
@@ -1,3 +1,34 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Others'),
+    ]
+
+    first_name = models.CharField(max_length=100, verbose_name='First Name')
+    last_name = models.CharField(max_length=100, verbose_name='Last Name')
+    birth_date = models.DateField(verbose_name='Date of birth')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name='Gender')
+
+    personal_id = models.CharField(
+        max_length=20,
+        unique=True,
+        verbose_name='Personal ID',
+        validators=[RegexValidator(r'^\d{9,12}$', 'Personal ID must be 9-12 digits')]
+    )
+    
+
+    address = models.TextField(verbose_name='Address')
+    phone = models.CharField(max_length=15, unique=True, verbose_name='Phone Number')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    registered_at = models.DateTimeField(auto_now_add=True, verbose_name='Registered At')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.personal_id})"
+
+
+
