8950f064150b9ed80ecdcd43941031f46ba868c5
David Rakovsky <david.rakovsky@centrum.cz>
2025-03-17 17:30:21 +0100

    Removed ignored files from tracking

--- asklepios/accounts/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def accounts(request):
-    return render(request, 'accounts/accounts.html')
-
-def privacy_policy(request):
-    return render(request, 'accounts/privacy_policy.html')
